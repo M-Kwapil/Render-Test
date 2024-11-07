84cc3de17873449ab3eaa4f32d63ccacb0055005
M-Kwapil <119266966+M-Kwapil@users.noreply.github.com>
2024-11-07 14:05:47 -0600

    Updating Backend to use MongoDB

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const app = express()
 
-app.use(express.static('dist'))
+const Note = require('./models/note')
+
 app.use(express.json())
 app.use(cors())
 
@@ -35,37 +37,32 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+        response.json(notes)
+    })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    const note = notes.find(note => note.id === id)
-    if (note) {
+    Note.findById(request.params.id).then(note => {
         response.json(note)
-    } else {
-        response.status(404).end()
-    }  
+    })
 })
 
 app.post('/api/notes', (request, response) => {
     const body = request.body
 
-    if (!body.content){
-        return response.status(400).json({
-            error: 'content missing'
-        })
+    if (body.content === undefined){
+        return response.status(400).json({ error: 'content missing' })
     }
     
-    const note = {
+    const note = new Note({
         content: body.content,
-        important: Boolean(body.importatnt) || false, 
-        id: generateId(),
-    }
+        important: body.importatnt || false, 
+    })
 
-    notes = notes.concat(note)
-    
-    response.json(note)
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -75,7 +72,7 @@ app.delete('/api/notes/:id', (request, response) => {
     response.status(204).end()
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
