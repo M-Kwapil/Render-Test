95bbc548f6d4cd005bb2e378c53bba31ca40a289
M-Kwapil <119266966+M-Kwapil@users.noreply.github.com>
2024-12-05 15:32:47 -0600

    Adding Error Handling for missing content in note

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.get('/api/notes/:id', (request, response, next) => {
 
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
     const body = request.body
 
     if (body.content === undefined){
@@ -44,9 +44,11 @@ app.post('/api/notes', (request, response) => {
         important: body.important || false, 
     })
 
-    note.save().then(savedNote => {
-        response.json(savedNote)
-    })
+    note.save()
+        .then(savedNote => {
+          response.json(savedNote)
+        })
+        .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
@@ -89,8 +91,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
-
+  } else if (error.name === 'ValidationError') {
+      return response.status(400).json({ error: error.message })
+  }
   next(error)
 }
 
